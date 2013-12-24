ff2ef3f39ca26ed5308b03f6836caf5f9c0b1125
Mahesh Babu Bokkisam <maheshbabu.bokkisam@gmail.com>
2013-12-24 17:44:38 +0530

    Fix the bugs

--- index.php
+++ index.php
@@ -33,10 +33,10 @@
 			.liveExample { height:20em; overflow:auto } /* Mobile Safari reflows pages slowly, so fix the height to avoid the need for reflows */
 
 			li { list-style-type: disc; margin-left: 20px; }
-			.action-status{color:green;}			
+			.action-status{color:green;}
 		</style>
 	</head>
-	
+
 	<body>
 		<div class="container">
 			<header class="navbar navbar-inverse navbar-fixed-top">
@@ -58,15 +58,15 @@
 									<input tabindex="1" id="txtUserName" name="txtUserName" type="text" required placeholder="Enter User name" class="form-control input-md" data-bind="value: txtUserName" data-required="true" data-msg_empty="Enter User name" />
 								</div>
 							</div>
-							
+
 							<!-- Text input-->
 							<div class="form-group">
 								<label class="col-md-4 control-label" for="txtContactNo">Contact No.</label>  
 								<div class="col-md-4">
-									<input tabindex="2" id="txtContactNo" name="txtContactNo" type="number" size="10" required placeholder="Enter Contact No." class="form-control input-md" pattern="[0-9]{10}" data-bind="value: txtContactNo" />
+									<input tabindex="2" id="txtContactNo" name="txtContactNo" type="text" size="10" required placeholder="Enter Contact No." class="form-control input-md" data-bind="value: txtContactNo" />
 								</div>
 							</div>
-							
+
 							<!-- Text input-->
 							<div class="form-group">
 								<label class="col-md-4 control-label" for="txtEmail">Email</label>
@@ -95,13 +95,13 @@
 									<textarea class="form-control" tabindex="6" id="txtAboutUser" name="txtAboutUser" data-bind="value:txtAboutUser">NA</textarea>
 								</div>
 							</div>
-							
+
 							<!-- Button (Double) -->
 							<div class="form-group">
 								<label class="col-md-4 control-label" for="button1id"></label>
 								<div class="col-md-8">
 									<button tabindex="7" type="submit" id="button1id" name="button1id" class="btn btn-success" data-bind="click:saveUserDetails">Submit</button>
-									<button tabindex="8" type="reset" id="button2id" name="button2id" class="btn btn-danger">Cancel</button>
+									<button tabindex="8" type="reset" id="button2id" name="button2id" class="btn btn-danger" data-bind="click:resetFormDetails">Cancel</button>
 								</div>
 							</div>
 							<div class="action-status" data-bind="html:actionStatus"></div>							
@@ -109,7 +109,7 @@
 					</form>	
 				</div>
 			</div>
-			
+
 			<div class="row">
 				<div class="span4">
 					<div class="" data-bind='simpleGrid: gridViewModel, simpleGridTemplate:"custom_grid_template"'></div>
@@ -123,17 +123,17 @@
 							</thead>
 							<tbody data-bind="foreach: itemsOnCurrentPage">
 							   <tr data-bind="foreach: $parent.columns">
-								   
+
 									<!--ko if: typeof rowText == 'object' && typeof rowText.action == 'function'-->
 										<!--ko if: actionText == 'Delete'-->
 										<td><button class="btn btn-danger" data-bind="click:rowText.action($parent)">Delete</button></td>
 										<!-- /ko -->
-										
+
 										<!--ko if: actionText == 'Edit'-->
 										<td><button class="btn btn-danger" data-bind="click:rowText.action($parent)">Edit</button></td>
 										<!-- /ko -->
 								    <!-- /ko -->
-								   
+
 								   <!--ko ifnot: typeof rowText == 'object' && typeof rowText.action == 'function'-->
 								   <td data-bind="text: typeof rowText == 'function' ? rowText($parent) : $parent[rowText] "></td>
 								   <!--/ko-->
@@ -141,12 +141,12 @@
 							</tbody>
 						</table>
 					</script>
-					
+
 					<button class="btn btn-success" data-bind='click: sortByName'>Sort by name</button>					
 				</div>
 			</div>
 		</div>
-		
+
 		<!-- Just for debugging purposes. Don't actually copy this line! -->
 		<!--[if lt IE 9]><script src="js/ie8-responsive-file-warning.js"></script><![endif]-->
 
@@ -155,42 +155,36 @@
 		  <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
 		  <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
 		<![endif]-->
-		    
+
 		<script type="text/javascript" src="js/jquery-1.11.0.js"></script>		
 		
 		<script type="text/javascript" src="js/bootstrap.js"></script>
 		
 		<script type="text/javascript" src="js/knockout-3.0.0.js"></script>
 		<script type="text/javascript" src="js/knockout.simpleGrid.3.0.js"></script>		
-		<script type="text/javascript" src="js/knockout.validation.js"></script>
 		
 		<script type="text/javascript">
-		jQuery(function() {			
-		
+		jQuery(function() {
 			//  If we are calling directly from php we can use this
 			//	var initialData = [<?php //echo $userDetailsObj; ?>];
 			//	var initialData = [ { name: "Optimistic Snail", sales: 420, price: 1.50 }, { name: "Optimistic Snail", sales: 420, price: 1.50 } ]
-			
-			ko.validation.configure({
-				insertMessages: false,
-				decorateElement: true,
-				// errorElementClass: 'error' 
-			});
-			
-			var vm = '';		
+
+			var vm = tmpItemObj = '';
 			
 			// Create a vew model method
-			var myViewModel = function(userDetails) {				
+			var myViewModel = function(userDetails) {
+		
+				var self = this;
 				
-				var self = this;	
-				
+				// Self Observables
+				{
 				// To detect and respond to changes of a collection of things
 				self.items	=	ko.observableArray(userDetails);
-				
-				/// BIND form values				
+
+				/// BIND form values
 				self.user_id		=	ko.observable();
 				self.hidUserId		=	self.user_id;
-				
+
 				self.txtUserName	=	ko.observable();
 				self.txtContactNo	=	ko.observable();
 				self.txtEmail		=	ko.observable();
@@ -198,24 +192,33 @@
 				self.txtAboutUser	=	ko.observable();
 				self.actionStatus	=	ko.observable('<span style="color:green">Loading user details...</span>');
 				self.ajaxStatus		=	ko.observable('');
+				}
 				
-				/// START VALIDATE AND SAVE	
+				// Start validate and save
 				self.saveUserDetails = function() {
 					self.User = {user_id:self.user_id(), txtUserName:self.txtUserName(), txtContactNo:self.txtContactNo(), txtEmail:self.txtEmail(), radGender:self.radGender(), txtAboutUser:self.txtAboutUser()};										
 					ajaxCall('insert', 'php_scripts/user_details.php', self.User);
-				};
-				/// END VALIDATE AND SAVE
+				};				
+
+				// Reset the form elements
+				self.resetFormDetails = function(){					
+					self.user_id('');  self.hidUserId(''); self.txtUserName(''); self.txtContactNo('');
+					self.txtEmail(''); self.radGender(''); self.txtAboutUser('');					
+				}
 				
+				// Load User Details when document loaded
 				self.loadUserDetails = function() {
 					ajaxCall('find', 'php_scripts/user_details.php', '');
 				};
-				
-				// Sort By name 	
+
+				// Sort By name
 				self.sortByName = function() {
 					self.items.sort(function(a, b) {
 						return a.name < b.name ? -1 : 1;
 					});
 				};
+				
+				// Start GRID view
 				self.gridViewModel = new ko.simpleGrid.viewModel({
 					data: self.items,
 					columns: [
@@ -227,21 +230,22 @@
 						{ 
 							headerText: "",
 							rowText: {
-								action: function(item){									
+								action: function(item){
 									return function(){
 										if(confirm("Are you sure you want Delete this record")){
-											ajaxCall('delete', 'php_scripts/user_details.php', item.user_id);
-										}	
+											ajaxCall('delete', 'php_scripts/user_details.php', item);
+										}
 									}
 								}
 							},
 							actionText:"Delete"
-						},						
+						},
 						{
 							headerText: "",
 							rowText: {
 								action: function(item){
 									return function(){
+										tmpItemObj	=	item;
 										//console.log('Edit');console.log(item);
 										self.hidUserId(item.user_id);
 										self.txtUserName(item.txtUserName);
@@ -258,15 +262,14 @@
 					],
 					pageSize: 10
 				});
-				/// END GRID VIEW				
 			};
-			
+
 			vm = new myViewModel( [] );
-			ko.applyBindings(vm);			
+			ko.applyBindings(vm);
 			
 			var ajaxCall	=	function (action, url, dataUser){
-				var data = (typeof dataUser == 'object') ? ko.toJS({"data":dataUser}) : ko.toJS( {"data":{'user_id':dataUser} });
-				
+				var data	=	ko.toJS({"data":dataUser});
+				vm.actionStatus((action == 'find') ? 'Loading Details...' : '');				
 				$.ajax({
 					crossDomain: true,
 					type: 'POST',
@@ -274,157 +277,65 @@
 					data: data,
 					dataType: 'json',
 					processdata: true,
-					success: function (result) {			
-					
-						// If action insert then insert user details in DB and update simpleGrid
+					success: function (result){
+						//	If action insert then insert user details in DB and update simpleGrid
 						if(action == 'insert'){
-							// Check weather hiddenUserId is empty or not, 
-							// If empty then it is new user details otherwise Update the details							
-							if(dataUser.user_id == undefined){
-								// Add new user details to items (either insert or update)
-								dataUser.user_id = result.userid;
-								console.log(vm.items()); console.log(dataUser);				
-								vm.items.push(dataUser);
-								vm.gridViewModel.currentPageIndex( parseInt( Math.ceil(vm.items().length/vm.gridViewModel.pageSize)-1) );
-								vm.actionStatus('<span style="color:green">Successfully inserted details</span>');
-							}else{
-								// Return user object position from usersArray using user_id/dataUser
-								var pos = vm.items().map(function(e) { return e.user_id; }).indexOf(vm.hidUserId());
-								
-								// flag is the position of object in array
-								if(pos >= 0 ){
-									//console.log(vm.items()[pos]);console.log(dataUser);
-									// Call compare objects method 
-									var flag = compareObjects(vm.items()[pos], dataUser);
-									//console.log(flag);
-									
-									if(flag === false) {
-										vm.items.remove(vm.items()[pos]);
-										vm.items.push(dataUser);
-									}
-									
-									vm.actionStatus('<span style="color:green">Successfully Updated details</span>');
-								}
-							}
-							
-							// Reset the form elements after Edit/Add
-							vm.hidUserId(''); vm.txtUserName(''); vm.txtContactNo('');
-							vm.txtEmail(''); vm.radGender(''); vm.txtAboutUser('');
-						}
-
-						// If action delete then delete user details from DB and update simpleGrid
-						else if(action == 'delete'){
 						
-							// Return user object position from usersArray using user_id/dataUser
-							var pos = vm.items().map(function(e) { return e.user_id; }).indexOf(dataUser);
-							
-							vm.items.remove(vm.items()[pos]);
-							vm.actionStatus('<span style="color:green">Successfully Deleted details</span>');
+							if(result.status) {
+								// Check weather hiddenUserId is empty or not, 
+								// If empty then it is new user details otherwise Update the details
+								if(dataUser.user_id == undefined || dataUser.user_id == ''){
+									
+									// Add new user details to items 
+									dataUser.user_id = result.returnData.userid;
+									
+									// console.log(vm.items()); console.log(dataUser);
+									vm.items.push(dataUser);
+									vm.gridViewModel.currentPageIndex( parseInt( Math.ceil(vm.items().length/vm.gridViewModel.pageSize)-1) );
+									vm.actionStatus('<span style="color:green">Successfully inserted details</span>');
+									
+								}
+								else{
+									vm.items.replace(tmpItemObj, dataUser);
+									tmpItemObj = '';
+								}
+								vm.resetFormDetails();
+							}
+							else{
+								vm.actionStatus('<span style="color:red">'+result.returnData+'</span>');
+							}							
 						}
 
-						// If action find then load user details and update simpleGrid
+						//	If action delete then delete user details from DB and update simpleGrid
+						else if(action == 'delete'){
+							if(result.status) {
+								vm.items.remove(dataUser);
+								vm.actionStatus('<span style="color:green">Successfully Deleted details</span>');
+							}else{
+								vm.actionStatus('<span style="color:red">'+result.returnData+'</span>');
+							}
+						}
+
+						//	If action find then load user details and update simpleGrid
 						else if(action == 'find'){
-							vm.actionStatus('<span style="color:green">Done.</span>');							
+							vm.actionStatus('<span style="color:green">Done.</span>');
 							
-							var userDetails = $.map(result, function(value, index) {
-								return [value];
-							});
+							// If we use Array functionality in php then we need this mapping
+							//var userDetails = $.map(result, function(value, index) { return [value]; });
 							
-							jQuery.each(userDetails, function(index, item){
-								vm.items.push(item);
-							});							
-							//vm = new myViewModel( userDetails );							
+							// If we use string json then we need to use ko utils parse JSON method
+							var userDetails = ko.utils.parseJson(result.returnData);
+							jQuery.each(userDetails, function(index, item){ vm.items.push(item); });							
 						}
 					},
-					error: function (xhr, ajaxOptions, thrownError) {						
+					error: function (xhr, ajaxOptions, thrownError) {
 						vm.actionStatus('<span style="color:red;">Error {Status: '+xhr.status+'}, {Options: '+ajaxOptions+'}, {Error: '+thrownError+'}</span>');
 					}
 				});
 			};
-			
-			//Returns the object's class, Array, Date, RegExp, Object are of interest to us
-			var getClass = function(val) {
-				return Object.prototype.toString.call(val).match(/^\[object\s(.*)\]$/)[1];
-			};
-			 
-			//Defines the type of the value, extended typeof
-			var whatis = function(val) {			 
-				if (val === undefined) { return 'undefined'; }
-				if (val === null){	return 'null'; }
-			 
-				var type = typeof val;
-			 
-				if (type === 'object') { type = getClass(val).toLowerCase(); }
-			 
-				if (type === 'number') {
-					if (val.toString().indexOf('.') > 0) { return 'float'; }
-					else { return 'integer'; }
-				}			 
-				return type;
-			};
-			 
-			/**
-			 *  Compare two Objects
-			 *  @parm a (object), b (object)
-			 *  @return flag false/true
-			 */
-			var compareObjects = function(a, b) {
-				if (a === b){ return true; }
-				for (var i in a) {
-					if (b.hasOwnProperty(i)) {
-						if (!equal(a[i],b[i])) { return false; }
-					} else { return false; }
-				}
-			 
-				for (var i in b) {
-					if (!a.hasOwnProperty(i)) { return false; }
-				}
-				return true;
-			};
-			
-			/**
-			 *  Compare two Arrays
-			 *  @parm a (array), b (array)
-			 *  @return flag false/true
-			 */
-			var compareArrays = function(a, b) {
-				if (a === b) { return true; }
-				if (a.length !== b.length) { return false; }
-				
-				for (var i = 0; i < a.length; i++){
-					if(!equal(a[i], b[i])) { return false; }
-				};
-				return true;
-			};
-			 
-			var _equal = {};
-			_equal.array = compareArrays;
-			_equal.object = compareObjects;
-			_equal.date = function(a, b) { return a.getTime() === b.getTime(); };
-			_equal.regexp = function(a, b) { return a.toString() === b.toString(); };
-			//	uncoment to support function as string compare
-			//	_equal.fucntion =  _equal.regexp; 			 
-			 
-			/*
-			 * Are two values equal, deep compare for objects and arrays.
-			 * @param a {any}
-			 * @param b {any}
-			 * @return {boolean} Are equal?
-			 */
-			var equal = function(a, b) {
-				if (a !== b) {
-					var atype = whatis(a), btype = whatis(b);
-			 
-					if (atype === btype && _equal.hasOwnProperty(atype))
-						return _equal[atype](a, b);
-			 
-					return false;
-				}			 
-				return true;
-			};
-			
+
 			// Load user details through AJAX call
-			vm.loadUserDetails();			
+			vm.loadUserDetails();
 		});
 		</script>
 	</body>
--- php_scripts/user_details.php
+++ php_scripts/user_details.php
@@ -1,76 +1,120 @@
 <?php
 //including common mongo connection file
-include('../mongo_connection.php');
+include('mongo_connection.php');
 /*
 //error handler function
-function customError($errno, $errstr){
-	echo "<b>Error:</b> [$errno] $errstr";
-}
+function customError($errno, $errstr){ echo "<b>Error:</b> [$errno] $errstr";}
 //set error handler
-set_error_handler("customError");*/
-
+set_error_handler("customError");
+*/
 // Selecting users_collection
 $collection = $database->selectCollection('users_collection');
 
-if($_REQUEST['action'] == 'insert') {
-	// Getting post text from the AJAX POST parameters
-	$data		=	$_POST['data'];
+/*
+// Bulk data enter
+for($i=0; $i<=100; $i++){
+	// Generating a new post id	
+	$user_id	=  new MongoId();
+	$timestamp	=  date('D, d-M-Y', $user_id->getTimestamp()+19800);
+	
+	$name 		=  'firstname lastname'.$i;
+	$email 		=  'firstnamelastname'.$i.'@gmail.com';
+	$contacno	=	88888888+$i;
+	$genderaray	=	array('male', 'female');	
+	$gender		=	$genderaray[rand(0,1)];
+	$aboutUser	=	'About User details of'.$name;
+	
+	$collection->insert(array ( '_id'=> $user_id, 'name' => $name, 'contact_no' => $contacno, 'email' => $email, 'gender' => $gender, 'comments' => $aboutUser, 'timestamp'=>$timestamp));
+}die();*/
 
-	// Generating a new post id
-	
-	$user_id	= ( isset($data['user_id']) && $data['user_id'] != '' ) ? new MongoId($data['user_id']) : new MongoId();
-	
-	// Generating post timestamp from the user id
-	// The statement should differ based on the date format needed and time zone 
-	$timestamp	=	date('D, d-M-Y', $user_id->getTimestamp()+19800);
-	$details	=	array( 'name' => $data['txtUserName'], 'contact_no' => $data['txtContactNo'], 'email' => $data['txtEmail'], 'gender' => $data['radGender'], 'comments' => $data['txtAboutUser'], 'timestamp'=>$timestamp);
-	
-	try {
-		if(isset($data['user_id']) && $data['user_id'] != ''){	
-			// UPDATE
-			$collection->update( array( '_id' => $user_id), array( '$set' => $details ) );		
+//$collection->remove();die();
+$status	=	false;
+$returnData	=	'';
+if($_REQUEST['action'] == 'insert') {	
+	try {		
+		// Getting post text from the AJAX POST parameters
+		if( isset($_POST['data']) ) {
+			$data		=	$_POST['data'];
+			
+			if( isset($data['txtUserName']) && $data['txtUserName'] != '' ) { }else{ throw new Exception('User Name is required'); break; }
+			if( isset($data['txtContactNo']) && $data['txtContactNo'] != '' ) {} else { throw new Exception('Contact No is required'); break; }
+			if( isset($data['txtEmail']) && $data['txtEmail'] != '' ) {} else  { throw new Exception('Email is required'); break; }
+			if( isset($data['radGender']) && $data['radGender'] != '' ) {} else  { throw new Exception('Gender is required'); break; }
+			if( isset($data['txtAboutUser']) && $data['txtAboutUser'] != '' ) {} else { throw new Exception('About User is required'); break; }
+		
+			if(isset($data['user_id']) && $data['user_id'] != ''){
+				// UPDATE
+				// Get user_id object for user_id
+				$user_id	= new MongoId($data['user_id']);
+				$collection->update( array( '_id' => $user_id), array( '$set' => array( 'name' => $data['txtUserName'], 'contact_no' => $data['txtContactNo'], 'email' => $data['txtEmail'], 'gender' => $data['radGender'], 'comments' => $data['txtAboutUser']) ) );
+				$returnData	=	array('userid'=>$user_id->{'$id'});
+			}else{
+				// INSERT
+				// Generate new mongoId for user_id
+				$user_id	= new MongoId();
+				// Generating post timestamp from the user id
+				// The statement should differ based on the date format needed and time zone 				
+				$timestamp	=	date('D, d-M-Y', $user_id->getTimestamp()+19800);
+				// Creates document for inserting new post
+				$collection->insert(array ( '_id'=> $user_id, 'name' => $data['txtUserName'], 'contact_no' => $data['txtContactNo'], 'email' => $data['txtEmail'], 'gender' => $data['radGender'], 'comments' => $data['txtAboutUser'], 'timestamp'=>$timestamp));				
+			}
+			$status		=	true;			
 		}else{
-			// Creates document for inserting new post			
-			$collection->insert(array ( '_id'=> $user_id, 'name' => $data['txtUserName'], 'contact_no' => $data['txtContactNo'], 'email' => $data['txtEmail'], 'gender' => $data['radGender'], 'comments' => $data['txtAboutUser'], 'timestamp'=>$timestamp));
-		}
-		echo json_encode(array('userid'=>$user_id->{'$id'}), JSON_FORCE_OBJECT);
+			throw new Exception('post data is not set');
+		}		
 	} catch(MongoCursorException $e) {
-		echo $e->getMessage();
-		echo $e->getCode();		
+		$returnData	=	"Error:".$e->getCode()." AND Message: ".$e->getMessage();
+	} catch(Exception $e){
+		$returnData	=	"Error:".$e->getCode()." AND Message: ".$e->getMessage();
 	}
+	
+	echo json_encode( array('returnData'=>$returnData, 'status'=>$status) );
 	
 }elseif( $_REQUEST['action'] == 'delete' ){
-	// Getting post text from the AJAX POST parameters
-	$data		=	$_POST['data'];
-
 	try {
-		$collection->remove(array("_id" => new MongoId($data['user_id']) ), array("justOne" => true));
-		echo json_encode(array('status'=>true), JSON_FORCE_OBJECT);		
+		// Getting post text from the AJAX POST parameters
+		if( isset($_POST['data']) ) {
+			$data		=	$_POST['data'];
+		
+			$collection->remove(array("_id" => new MongoId($data['user_id']) ), array("justOne" => true));
+			$status	=	true;
+		}else{
+			throw new Exception('post data is not set');
+		}
 	} catch(MongoCursorException $e) {
-		echo $e->getMessage();
-		echo $e->getCode();
+		$returnData	=	"Error:".$e->getCode()." AND Message: ".$e->getMessage();
+	} catch(Exception $e){
+		$returnData	=	"Error:".$e->getCode()." AND Message: ".$e->getMessage();
 	}
+	echo json_encode( array('returnData'=>$returnData, 'status'=>$status));
+	
 }elseif( $_REQUEST['action'] == 'find' ){
 	try {
 		// Retrieving all the posts in the collection
 		// If you want to retrieve specific posts based on user, relations, etc. put filter condition in find 
 		$users_cursor=$collection->find()->sort(array('_id'=>-1));
 		
-		$userDetailsObj	=	'';
+		$userDetailsObj	=	'[';	// sending as array no need to add "[" bracket
 		
 		//Iterating over all the retrieved posts
 		while ($users_cursor->hasNext()) { 
 			$user = $users_cursor->getNext();
 			// this step useful when we are calling directly from php	
-			//$userDetailsObj .=	"{user_id:'".$user['_id']->{'$id'}."', txtUserName:'".$user['name']."', txtContactNo:'".$user['contact_no']."', txtEmail:'".$user['email']."', radGender:'".$user['gender']."', txtAboutUser:'".$user['comments']."'},";
+			$userDetailsObj .=	'{"user_id":"'.$user['_id']->{'$id'}.'", "txtUserName":"'.$user['name'].'", "txtContactNo":"'.$user['contact_no'].'", "txtEmail":"'.$user['email'].'", "radGender":"'.$user['gender'].'", "txtAboutUser":"'.$user['comments'].'"},';
+			
 			// this step is useful for AJAX return variable 
-			$userDetailsObj[] =	 array('user_id'=>$user['_id']->{'$id'}, 'txtUserName'=>$user['name'], 'txtContactNo'=>$user['contact_no'], 'txtEmail'=>$user['email'], 'radGender'=>$user['gender'], 'txtAboutUser'=>$user['comments'] );			
-		}		
-		header('Content-Type: application/json');		
-		echo json_encode( $userDetailsObj, JSON_FORCE_OBJECT );		
+			//$userDetailsObj[] =	 array('user_id'=>$user['_id']->{'$id'}, 'txtUserName'=>$user['name'], 'txtContactNo'=>$user['contact_no'], 'txtEmail'=>$user['email'], 'radGender'=>$user['gender'], 'txtAboutUser'=>$user['comments'] );			
+		}
+		$returnData	=	rtrim($userDetailsObj, ",").']'; // sending as array no need to add "]" bracket
+		$status	=	true;
 	} catch(MongoCursorException $e) {
-		echo $e->getMessage();
-		echo $e->getCode();
-	}	
+		$returnData	=	"Error:".$e->getCode()." AND Message: ".$e->getMessage();
+	}
+	
+	echo json_encode( array('returnData'=>$returnData, 'status'=>$status) );
+}
+
+function findId($userId){
+	return $collection->find(array("_id" => $userId))->count();
 }
 ?>
