f24d3e79981376cef39be8849d24d42001f23655
Mahesh Babu Bokkisam <maheshbabu.bokkisam@gmail.com>
2013-12-30 11:54:06 +0530

    Update mongo_connection.php

--- php_scripts/mongo_connection.php
+++ php_scripts/mongo_connection.php
@@ -1,5 +1,4 @@
 <?php
-/*Code written by Ashish Trivedi*/
 
 //This file contains the common database connection code used accross files
 //If you have modified username and password for connection, define it here     
